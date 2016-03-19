41b3ff2cde4019cfe1db1d372da0e3f46e8d5ba9
kanasimi <kanasimi@gmail.com>
2016-03-19 10:21:00 +0800

    endding(), escape ',', '"' of title

--- process_dump.js
+++ process_dump.js
@@ -1,12 +1,6 @@
 ﻿// cd ~/wikibot && time ../node/bin/node process_dump.js
 
-/*
-
- 2016/3/12 11:56:10	初版試營運
- 上路前修正
- 完善
-
- */
+// 2016/3/12 11:56:10	初版試營運
 
 'use strict';
 
@@ -16,30 +10,31 @@ CeL.run('application.platform.nodejs');
 
 function import_data(error) {
 	if (error)
-		console.error(error);
+		CeL.err(error);
 
+	var start_read_time = Date.now(), count = 0, max_length = 0;
 	CeL.wiki.read_dump(function(page_data, filename) {
 		if (++count % 10000 === 0)
 			// e.g., "2660000: 16.546 page/ms Wikipedia:优良条目/2015年8月23日"
 			CeL.log(count + ': '
-					+ (count / (Date.now() - start_time)).toFixed(3)
+					+ (count / (Date.now() - start_read_time)).toFixed(3)
 					+ ' page/ms\t' + page_data.title);
 		// var title = page_data.title, content = page_data.revisions[0]['*'];
 		// 似乎沒這種問題。
 		if (false && !page_data.title)
 			CeL.warn('* No title: [[' + page_data.id + ']]');
 		// [[Wikipedia:快速删除方针]]
-		if (!page_data.revisions[0]['*'])
+		if (page_data.revisions[0]['*'])
+			max_length = Math.max(max_length, page_data.revisions[0]['*'].length);
+		else
 			CeL.warn('* No content: [[' + page_data.title + ']]');
 
 		// Write to .csv
 
-		var title = page_data.title;
-		if (title.includes('"'))
-			// escape
-			title = '"' + title.replace(/"/g, '""') + '"';
 		if (do_write_file) {
-			file_stream.write([ page_data.pageid, page_data.ns, title,
+			file_stream.write([ page_data.pageid, page_data.ns,
+			// escape ',', '"'
+			'"' + page_data.title.replace(/"/g, '""') + '"',
 			// '2000-01-01T00:00:00Z' → '2000-01-01 00:00:00'
 			page_data.revisions[0].timestamp.slice(0, -1).replace('T', ' '),
 			//
@@ -60,7 +55,7 @@ function import_data(error) {
 				page_data.revisions[0]['*'] ]
 			}, function(error) {
 				if (error)
-					console.error(error);
+					CeL.err(error);
 			});
 	}, {
 		directory : bot_directory + 'dumps/',
@@ -71,31 +66,44 @@ function import_data(error) {
 				file_stream = new require('fs').WriteStream(filename, 'utf8');
 		},
 		last : function() {
+			// e.g., "All 2755239 pages, 167.402 s."
+			CeL.log('All ' + count + ' pages, ' + (Date.now() - start_read_time)
+					/ 1000 + ' s. Max page length: ' + max_length);
+
 			if (do_write_file) {
 				file_stream.end();
+
 				if (!do_realtime_import) {
 					setup_SQL(function(error) {
 						if (error)
-							console.error(error);
+							CeL.err(error);
 
-						connection.query("LOAD DATA LOCAL INFILE '" + file_stream.path
+						CeL.info('Import data to database...');
+						var SQL = "LOAD DATA LOCAL INFILE '" + file_stream.path
 						//
-						+ "' INTO TABLE `page` FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n' (pageid,ns,title,timestamp,text);");
+						+ "' INTO TABLE `page` FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n' (pageid,ns,title,timestamp,text);";
+						CeL.log(SQL);
+						connection.query(SQL, function(error, rows) {
+							if (error)
+								CeL.err(error);
+							else
+								CeL.log(rows);
+							endding();
+						});
 					});
 				}
-			}
-			// e.g., "All 2755239 pages, 167.402 s."
-			CeL.log('All ' + count + ' pages, ' + (Date.now() - start_time)
-					/ 1000 + ' s.');
+			} else
+				endding();
 		}
 	});
 }
 
 
 function setup_SQL(callback) {
+	CeL.info('Re-create database...');
 	SQL_session = new CeL.wiki.SQL('zhwiki', function(error) {
 		if (error)
-			console.error(error);
+			CeL.err(error);
 
 		connection.query('DROP TABLE `page`', function(error) {
 			connection.query(create_SQL, callback);
@@ -105,10 +113,13 @@ function setup_SQL(callback) {
 	connection = SQL_session.connection;
 }
 
+function endding() {
+	CeL.log('All ' + (Date.now() - start_read_time) / 1000 / 60 + ' min.');
+}
 
-var start_time = Date.now(), count = 0,
+var start_time = Date.now()
 /** {Boolean}import to database */
-do_realtime_import,
+do_realtime_import = false,
 // 使用新版 node.js 能加快速度，降低 CPU 與 RAM 使用，使用時間約需近 20 min。
 /** {Boolean}write to CSV file. */
 do_write_file = true, file_stream,
@@ -123,7 +134,7 @@ SQL_session, connection;
 if (do_realtime_import) {
 	setup_SQL(function(error) {
 		if (error)
-			console.error(error);
+			CeL.err(error);
 
 		// FATAL ERROR: JS Allocation failed - process out of memory
 		// Aborted
@@ -132,14 +143,3 @@ if (do_realtime_import) {
 } else {
 	import_data();
 }
-
-/**
- * <code>
- LOAD DATA INFILE 'dumps/zhwiki-20160305-pages-articles.csv'
- INTO TABLE `page`
- FIELDS TERMINATED BY ','
- ENCLOSED BY '"'
- LINES TERMINATED BY '\n'
- IGNORE 1 ROWS;
- </code>
- */
