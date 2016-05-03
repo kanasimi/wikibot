442a3e3f363762572266a6b5a2bb9147f9a682ae
kanasimi <kanasimi@gmail.com>
2016-05-03 17:24:54 +0800

    未完成，

--- 20160414.import_label_from_wiki_link.js
+++ 20160414.import_label_from_wiki_link.js
@@ -81,11 +81,11 @@ var
 /** {Natural}所欲紀錄的最大筆數。 */
 log_limit = 4000,
 //
-count = 0, test_limit = 1600,
+count = 0, test_limit = Infinity,
 //
 use_language = 'zh', data_file_name = 'labels.json',
 // 是否要使用Wikidata數據來清理跨語言連結。
-modify_Wikipedia = true;
+modify_Wikipedia = false;
 
 // ----------------------------------------------------------------------------
 
