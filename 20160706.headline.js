5c82be17b132798b19b5156d6fb95da9aca928d1
kanasimi <kanasimi@gmail.com>
2016-07-08 10:07:03 +0800

    fix 晚報

--- 20160706.headline.js
+++ 20160706.headline.js
@@ -25,6 +25,9 @@ headline_sites = {
 
 	// [ query, 擷取數 [標題關鍵字] ]
 	'中央社商情網' : [ '"%Y年%m月%d日" "頭條新聞標題" site:www.cnabc.com', [ '日報', '晚報' ] ],
+	// 不知為何，Google 得要用這樣的方法才查得到晚報頭條。
+	'中央通訊社' : '台灣主要晚報頭條新聞標題 %Y年 %m月 %d日',
+
 	'中央社' : '"%m月%d日" "各報頭條" site:www.cna.com.tw',
 	'蘋果日報 (台灣)' : [ '"%4Y%2m%2d" "各報頭條搶先報" site:appledaily.com.tw',
 			[ '世界各報頭條', '各報頭條' ] ],
@@ -37,7 +40,9 @@ headline_sites = {
 //
 add_source_data = [],
 
-use_date = new Date(2016, 7 - 1, 6);
+use_date = new Date;
+
+// use_date = new Date(2016, 7 - 1, 8);
 
 // ---------------------------------------------------------------------//
 
@@ -56,26 +61,44 @@ function check_finish(sites_to_check) {
 		add_source_data.push('<!-- Error: ' + site + ' -->');
 	}
 
+	// assert: 已設定好 page
 	wiki.edit(function(page_data) {
 		add_source_data = add_source_data.join('\n');
+		/** {Number}一整天的 time 值。should be 24 * 60 * 60 * 1000 = 86400000. */
+		var ONE_DAY_LENGTH_VALUE = new Date(0, 0, 2) - new Date(0, 0, 1),
+
+		// 前一天, the day before
+		day_before = new Date(use_date.getTime() - ONE_DAY_LENGTH_VALUE),
+		// 後一天, 隔天 the day after
+		day_after = new Date(use_date.getTime() + ONE_DAY_LENGTH_VALUE);
+
+		function headline_link(date, add_year) {
+			return '[[' + date.format('%Y年%m月%d日') + '臺灣報紙頭條|'
+					+ date.format(add_year ? '%Y年%m月%d日' : '%m月%d日') + ']]';
+		}
+
 		var content = CeL.wiki.content_of(page_data)
-				// 初始模板。
-				|| ('{{date|' + use_date.format('%Y年%m月%d日')
-				//
-				+ '}}\n\n以下為[[w:民國紀年|民國]]105年7月5日[[w:臺灣|臺灣]]報紙頭條：\n'
-				//
-				+ '\n== 消息來源 ==\n\n\n{{develop}}'
-				//
-				+ '\n{| class="wikitable" style="text-align: center;"'
-				// TODO
-				+ '\n|+\'\'\'臺灣報紙頭條\'\'\'\n|-\n|←[[2016年7月4日臺灣報紙頭條|7月4日]]'
-				//
-				+ '||[[' + use_date.format('%Y年%m月%d日')
-						+ '臺灣報紙頭條|2016年7月5日]]||[[2016年7月6日臺灣報紙頭條|7月6日]]→\n|}'
-						//
-						+ '\n[[Category:報紙頭條]]\n[[Category:臺灣]]'
-				//
-				+ '\n[[Category:2016年7月臺灣報紙頭條]]');
+		// 初始模板。
+		|| ('{{date|' + use_date.format('%Y年%m月%d日')
+		//
+		+ '}}\n\n以下為[[w:民國紀年|民國]]' + use_date.format({
+			format : '%R年%m月%d日',
+			locale : 'cmn-Hant-TW'
+		})
+		//
+		+ '[[w:臺灣|臺灣]]報紙頭條：\n\n== 消息來源 ==\n\n\n{{develop}}\n'
+		//
+		+ '{| class="wikitable" style="text-align: center;"\n'
+		//
+		+ '|+\'\'\'臺灣報紙頭條\'\'\'\n|-\n|←' + headline_link(day_before)
+		//
+		+ '||' + headline_link(use_date, true)
+		//
+		+ '||' + headline_link(day_after) + '→\n|}\n'
+		//
+		+ '[[Category:報紙頭條]]\n[[Category:臺灣]]\n'
+		//
+		+ '[[Category:' + use_date.format('%Y年%m月') + '臺灣報紙頭條]]');
 		content = content.replace(/(\n|^)==\s*消息來源\s*==\n/, function(section) {
 			section += add_source_data;
 			add_source_data = null;
@@ -235,7 +258,7 @@ wiki.page(use_date.format('%Y年%m月%d日') + '臺灣報紙頭條', function(pa
 
 	function for_each_template(token, token_index, token_parent) {
 		var template_name = token.name.toLowerCase();
-		console.log(token);
+		// console.log(token);
 		if (template_name === 'source' && token.parameters.url
 				&& token.parameters.title
 				&& (token.parameters.pub in sites_to_check)) {
