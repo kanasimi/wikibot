bdbd9271bd8da513ac54a4a57c20162cb1a57fcf
kanasimi <kanasimi@gmail.com>
2016-07-08 17:49:29 +0800

    headline_labels

--- 20160706.headline.js
+++ 20160706.headline.js
@@ -17,16 +17,26 @@ var
 /** {Object}wiki operator 操作子. */
 wiki = Wiki(true, 'wikinews'),
 
+url_hash = CeL.null_Object(),
+
 // 須改 cx!!
-headline_sites = {
+headline_labels = {
+	// label : {String}query
+	// label : [ {String}query, 擷取數 [標題關鍵字] ]
+	// label : [ {String}query, 擷取數 [標題關鍵字], publisher ]
+
 	// cn
 	// http://finance.sina.com.cn/stock/y/2016-07-06/doc-ifxtsatn8182961.shtml
 	// http://finance.eastmoney.com/news/1353,20160706639330278.html
 
-	// [ query, 擷取數 [標題關鍵字] ]
-	'中央社商情網' : [ '"%Y年%m月%d日" "頭條新聞標題" site:www.cnabc.com', [ '日報', '晚報' ] ],
+	// 中央社商情網 商情新聞中心
+	'中央社商情網' : [ '"%Y年%m月%d日" "頭條新聞標題" site:www.cnabc.com', [ '日報'
+	// 台灣主要晚報頭條新聞標題
+	// , '晚報'
+	] ],
 	// 不知為何，Google 得要用這樣的方法才查得到晚報頭條。
-	'中央通訊社' : '台灣主要晚報頭條新聞標題 %Y年 %m月 %d日',
+	// '中央通訊社' : '台灣主要晚報頭條新聞標題 %Y年 %m月 %d日',
+	'中央社商情網晚報' : [ '台灣主要晚報頭條新聞標題 %Y年 %m月 %d日', , '中央社商情網' ],
 
 	'中央社' : '"%m月%d日" "各報頭條" site:www.cna.com.tw',
 	'蘋果日報 (台灣)' : [ '"%4Y%2m%2d" "各報頭條搶先報" site:appledaily.com.tw',
@@ -49,7 +59,7 @@ use_date = new Date;
 // 這可能需要十幾秒。
 var google = require('googleapis'), customsearch = google.customsearch('v1');
 
-function check_finish(sites_to_check) {
+function check_finish(labels_to_check) {
 	if (add_source_data.length === 0) {
 		// 沒有新資料，或者全部錯誤。
 		return;
@@ -57,8 +67,8 @@ function check_finish(sites_to_check) {
 
 	add_source_data.sort();
 
-	for ( var site in sites_to_check) {
-		add_source_data.push('<!-- Error: ' + site + ' -->');
+	for ( var label in labels_to_check) {
+		add_source_data.push('<!-- Error: ' + label + ' -->');
 	}
 
 	// assert: 已設定好 page
@@ -117,7 +127,14 @@ function check_finish(sites_to_check) {
 }
 
 // return 有去掉/已處理過。
-function remove_completed(sites_to_check, site, title) {
+function remove_completed(labels_to_check, label, title, url) {
+	if (!title || typeof title !== 'string' || !(label in labels_to_check))
+		return;
+
+	CeL.log('remove_completed: 登記已處理過 ' + title
+	//
+	+ (url ? ': ' + url : '') + '。');
+
 	if (!/[頭头][版條条]/.test(title)) {
 		return;
 	}
@@ -135,59 +152,91 @@ function remove_completed(sites_to_check, site, title) {
 		return;
 	}
 
-	var site_data = sites_to_check[site];
-	if (Array.isArray(site_data)) {
-		// assert: Array.isArray(site_data[1])
-		CeL.debug('檢查標題關鍵字: [' + site_data[1] + '] in "' + title + '"', 0,
+	var not_added = true;
+	if (url) {
+		// 登記url，以避免重複加入url。
+		if (url_hash[url]) {
+			not_added = false;
+			CeL.log('remove_completed: 重複處理了 [' + url + ']: ' + url_hash[url]
+					+ ' & ' + title + '.');
+		} else {
+			url_hash[url] = title;
+		}
+	}
+
+	// 登記並去除已處理之label。
+	var label_data = labels_to_check[label];
+	if (Array.isArray(label_data)) {
+		if (!label_data[1]) {
+			// label : [ {String}query, , publisher ]
+			// →
+			// label : {String}query
+			label_data = label_data[0];
+		} else if (!Array.isArray(label_data[1])) {
+			// label : [ {String}query, 標題關鍵字 ]
+			// →
+			// label : [ {String}query, [標題關鍵字] ]
+			label_data[1] = [ label_data[1] ];
+		}
+	}
+
+	if (Array.isArray(label_data)) {
+		// label : [ {String}query, [標題關鍵字] ]
+		// assert: Array.isArray(label_data[1])
+		CeL.debug('檢查標題關鍵字: [' + label_data[1] + '] in "' + title + '"', 0,
 				'remove_completed');
-		return site_data[1].some(function(key, index) {
+		return label_data[1].some(function(key, index) {
 			if (title.includes(key)) {
-				if (site_data[1].length === 1) {
-					CeL.debug('[' + site + ']: 到齊了。', 0, 'remove_completed');
-					delete sites_to_check[site];
+				if (label_data[1].length === 1) {
+					CeL.debug('[' + label + ']: 到齊了。', 0, 'remove_completed');
+					delete labels_to_check[label];
 				} else {
-					CeL.debug('[' + site + ']: 去掉本 key [' + key + ']。', 0,
+					CeL.debug('[' + label + ']: 去掉本 key [' + key + ']。', 0,
 							'remove_completed');
-					site_data[1].splice(index, 1);
+					label_data[1].splice(index, 1);
 				}
 				return true;
 			}
-		});
+		})
+				&& not_added;
 	}
 
-	CeL.debug('[' + site + ']: 標注已處理過 [' + title + ']。', 0, 'remove_completed');
-	delete sites_to_check[site];
-	return true;
+	// label : {String}query
+	CeL
+			.debug('[' + label + ']: 標注已處理過 [' + title + ']。', 0,
+					'remove_completed');
+	delete labels_to_check[label];
+	return not_added;
 }
 
-function check_sites(sites_to_check) {
-	var sites = Object.keys(sites_to_check),
+function check_labels(labels_to_check) {
+	var labels = Object.keys(labels_to_check),
 	// 剩下 remain
-	left = sites.length;
+	left = labels.length;
 
 	if (!left) {
-		check_finish(sites_to_check);
+		check_finish(labels_to_check);
 		return;
 	}
 
-	function for_site(site, error, response) {
+	function for_label(label, error, response) {
 		left--;
 		if (error) {
-			CeL.err('for_site: [' + site + ']: error: ' + error);
+			CeL.err('for_label: [' + label + ']: error: ' + error);
 			if (!left) {
-				check_finish(sites_to_check);
+				check_finish(labels_to_check);
 			}
 			return;
 		}
 
-		CeL.log('for_site: Search Google for [' + site + ']: '
+		CeL.log('for_label: Search Google for [' + label + ']: '
 				+ response.searchInformation.formattedTotalResults
 				+ ' results.');
 		if (!response.items || response.items.length === 0) {
-			CeL.log('for_site: [' + site + ']: No data get: '
+			CeL.log('for_label: [' + label + ']: No data get: '
 					+ JSON.stringify(response.url));
 			if (!left) {
-				check_finish(sites_to_check);
+				check_finish(labels_to_check);
 			}
 			return;
 		}
@@ -199,54 +248,67 @@ function check_sites(sites_to_check) {
 		// "htmlTitle":"...","link":"http://www.cnabc.com/news/aall/201607050083.aspx","displayLink":"www.cnabc.com","snippet":"...","htmlSnippet":"...","cacheId":"BlsHSMJeb9AJ","formattedUrl":"www.cnabc.com/news/aall/201607050083.aspx","htmlFormattedUrl":"www.cnabc.com/news/aall/201607050083.aspx",
 		// "pagemap":{"metatags":[{"viewport":"width=device-width,initial-scale=1.0","og:title":"...","og:image":"http://img1.cna.com.tw/cbp/images/pic_fb.jpg","og:url":"http://www.cnabc.com/news/aall/201607050083.aspx","og:type":"article"}],"cse_image":[{"src":"http://img1.cna.com.tw/cbp/images/pic_fb.jpg"}]}}
 		function add_source(item) {
-			if (remove_completed(sites_to_check, site, item.title)) {
-				add_source_data.push('* {{source|url=' + item.link + '|title='
-						+ item.title.replace(/[\s\|]+/g, ' ') + '|author='
-						+ site + '|pub=' + site + '|date='
-						+ use_date.format('%Y年%m月%d日') + '}}');
+			if (remove_completed(labels_to_check, label, item.title, item.link)) {
+				// label : [ {String}query, 擷取數 [標題關鍵字], publisher ]
+				var publisher = Array.isArray(labels_to_check[label])
+						&& labels_to_check[label][2] || label;
+				// add {{Template:source}}
+				add_source_data.push('* {{source|url=' + item.link
+				//
+				+ '|title=' + item.title.replace(/[\s\|]+/g, ' ')
+				//
+				+ '|author=' + publisher
+				//
+				+ '|pub=' + publisher
+				//
+				+ '|date=' + use_date.format('%Y年%m月%d日')
+				//
+				+ (label === publisher ? '' : '|label=' + label) + '}}');
+
 			} else {
 				CeL.err('add_source: error title: [' + item.title + '] ['
 						+ item.link + ']');
 				console.log(item);
 			}
-			CeL.debug('add_source: [' + site + ']: '
+			CeL.debug('add_source: [' + label + ']: '
 			//
-			+ (sites_to_check[site] ? '尚存有未處理資料，將持續處理下去。' : '已無未處理資料，將跳出此項。'),
-					0, 'add_source');
-			return sites_to_check[site];
+			+ (labels_to_check[label] ? '尚存有未處理資料，將持續處理下去。'
+			//
+			: '已無未處理資料，將跳出此項。'), 0, 'add_source');
+			return labels_to_check[label];
 		}
 
 		response.items.every(add_source);
 
 		if (!left) {
-			check_finish(sites_to_check);
+			check_finish(labels_to_check);
 		}
 	}
 
-	sites.forEach(function(site) {
+	labels.forEach(function(label) {
 		// query string
-		var query = sites_to_check[site];
+		var query = labels_to_check[label];
 		if (Array.isArray(query)) {
 			query = query[0];
 		}
 		query = use_date.format(query);
-		CeL.debug('check_sites: Search Google for [' + site + ']: [' + query
-				+ ']', 0, 'check_sites');
+		CeL.debug('check_labels: Search Google for [' + label + ']: [' + query
+				+ ']', 0, 'check_labels');
 
 		// https://github.com/google/google-api-nodejs-client/blob/master/samples/customsearch/customsearch.js
 		customsearch.cse.list(Object.assign({
 			q : query
 		}, API_code), function(error, response) {
-			for_site(site, error, response);
+			for_label(label, error, response);
 		});
 	});
 
 }
 
 wiki.page(use_date.format('%Y年%m月%d日') + '臺灣報紙頭條', function(page_data) {
-	var sites_to_check = Object.clone(headline_sites, true);
+	var labels_to_check = Object.clone(headline_labels, true);
 	if (!page_data || ('missing' in page_data)) {
-		check_sites(sites_to_check);
+		check_labels(labels_to_check);
 		return;
 	}
 
@@ -259,16 +321,14 @@ wiki.page(use_date.format('%Y年%m月%d日') + '臺灣報紙頭條', function(pa
 	function for_each_template(token, token_index, token_parent) {
 		var template_name = token.name.toLowerCase();
 		// console.log(token);
-		if (template_name === 'source' && token.parameters.url
-				&& token.parameters.title
-				&& (token.parameters.pub in sites_to_check)) {
-			CeL.log('for_each_template: 已處理過 ' + token.parameters.pub + '。');
-			remove_completed(sites_to_check, token.parameters.pub,
-					token.parameters.title);
+		if (template_name === 'source' && token.parameters.url) {
+			remove_completed(labels_to_check, token.parameters.label
+					|| token.parameters.pub, token.parameters.title,
+					token.parameters.url);
 		}
 	}
 
 	parser.each('template', for_each_template);
-	// console.log(sites_to_check);
-	check_sites(sites_to_check);
+	// console.log(labels_to_check);
+	check_labels(labels_to_check);
 });
