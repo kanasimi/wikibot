a1b425dc7bf7eff44fae3c11302b74a02ebceb0b
kanasimi <kanasimi@gmail.com>
2016-07-14 23:54:38 +0800

    function for_each_page(page_data)

--- 20160714.archive_news.js
+++ 20160714.archive_news.js
@@ -14,7 +14,18 @@ CeL.run('data.math');
 
 var
 /** {Object}wiki operator 操作子. */
-wiki = Wiki(true, 'wikinews');
+wiki = Wiki(true, 'wikinews'),
+
+/** {Number}一整天的 time 值。should be 24 * 60 * 60 * 1000 = 86400000. */
+ONE_DAY_LENGTH_VALUE = new Date(0, 0, 2) - new Date(0, 0, 1),
+
+// [[Wikinews:存檔常規]]: 已發表兩週或以上的文章會列入存檔並且可被保護。
+time_limit = Date.now() - 14 * ONE_DAY_LENGTH_VALUE,
+
+// ((Infinity)) for do all
+test_limit = 2,
+
+page_list = [];
 
 // ----------------------------------------------------------------------------
 
@@ -25,6 +36,7 @@ function check_redirect_to(template_name_hash, callback) {
 				page_data) {
 			// console.log(page_data.response.query);
 			// console.log(redirect_data);
+			CeL.info(template_name + ' →	' + redirect_data);
 			template_name_hash[template_name] = redirect_data;
 			if (--left === 0) {
 				callback(template_name_hash);
@@ -44,7 +56,8 @@ function main_work(template_name_redirect_to) {
 		list : template_name_redirect_to.published,
 		reget : true,
 		operator : function(list) {
-			this.published = list.map(page_data_to_String);
+			this.published = list;
+			// this.published = list.map(page_data_to_String);
 		}
 
 	}, {
@@ -52,20 +65,27 @@ function main_work(template_name_redirect_to) {
 		list : template_name_redirect_to.archived,
 		reget : true,
 		operator : function(list) {
-			this.archived = list.map(page_data_to_String);
+			this.archived = list;
+			// this.archived = list.map(page_data_to_String);
 		}
 
 	} ], function() {
+		CeL.log(this.archived.length + ' archived, ' + this.published.length
+				+ ' published.');
 		// 取差集: 從比較小的來處理。
-		this.archived = CeL.get_set_complement(this.published, this.archived);
-		this.published = this.archived[0];
-		this.archived = this.archived[1];
+		var list = CeL.get_set_complement(this.published, this.archived);
 
-		console.log(this.archived.length);
-		console.log(this.published.length);
-		throw 1;
+		CeL.log('→ ' + list[1].length + ' archived, ' + list[0].length
+				+ ' published.');
 
-		var list = this.list;
+		if (list[1].length) {
+			// 依照現在 {{publish}} 的寫法，不應出現此項。
+			CeL.err('有' + list[1].length + '個已存檔，但沒有發布之條目！');
+		}
+
+		list = list[0];
+
+		// var list = this.list;
 		// list = [ '' ];
 		CeL.log('Get ' + list.length + ' pages.');
 		if (0) {
@@ -76,14 +96,13 @@ function main_work(template_name_redirect_to) {
 			list = list.slice(0 * test_limit, 1 * test_limit);
 		}
 
-		// setup ((page_remains))
-		page_remains = list.length;
 		wiki.work({
 			no_edit : true,
 			each : for_each_page,
 			page_options : {
-				rvprop : 'ids|content|timestamp'
-			}
+				rvprop : 'ids|timestamp'
+			},
+			last : archive_page
 
 		}, list);
 
@@ -97,6 +116,18 @@ function main_work(template_name_redirect_to) {
 		prefix : base_directory
 	});
 
+}
+
+function for_each_page(page_data) {
+	// console.log(page_data);
+	if (Date.parse(page_data.revisions[0].timestamp) < time_limit) {
+		page_list.push(page_data);
+	}
+}
+
+function archive_page() {
+	CeL.log('存檔 ' + page_list.length + ' 文章');
+	console.log(page_list.slice(0, 9));
 }
 
 // ----------------------------------------------------------------------------
