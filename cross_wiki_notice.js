2e70415bde7fa981c6ed33bf5da4cd1c44ff0a37
kanasimi <kanasimi@gmail.com>
2016-10-27 19:19:15 +0800

    常態性運行。

--- cross_wiki_notice.js
+++ cross_wiki_notice.js
@@ -1,6 +1,7 @@
 ﻿/*
 
  2016/10/23 20:34:46	初版試運行。
+ 2016/10/27 19:17:15	常態性運行。
 
  */
 
@@ -17,7 +18,7 @@ wiki = Wiki(true, 'https://zh.moegirl.org/api.php');
 
 // CeL.set_debug(2);
 if (false) {
-	wiki.page('User_talk:Kanashimi', function(page_data) {
+	wiki.page('User talk:Kanashimi', function(page_data) {
 		date_list = CeL.wiki.parse.date(CeL.wiki.content_of(page_data), true,
 				true);
 		max = Math.max.apply(null, date_list);
@@ -25,7 +26,7 @@ if (false) {
 	});
 }
 
-wiki.page('User_talk:' + owner_name, function(page_data) {
+wiki.page('User talk:' + owner_name, function(page_data) {
 	var revision = CeL.wiki.content_of.revision(page_data),
 	//
 	user = revision && CeL.wiki.normalize_title(revision.user);
@@ -39,7 +40,7 @@ wiki.page('User_talk:' + owner_name, function(page_data) {
 	CeL.log('新留言: ' + user + ' @ ' + revision.timestamp);
 
 	var zhwiki = Wiki(true, 'zh');
-	zhwiki.page('User_talk:' + owner_name, check_talk_page);
+	zhwiki.page('User talk:' + owner_name, check_talk_page);
 
 	function check_talk_page(page_data) {
 		var
@@ -55,15 +56,15 @@ wiki.page('User_talk:' + owner_name, function(page_data) {
 		}
 
 		zhwiki.edit(': ' + user + ' 於 ' + revision.timestamp
-				+ ' 在 [https://zh.moegirl.org/User_talk:' + owner_name
-				+ ' moegirl] 發了一則新留言，煩請撥空前往查看。 --~~~~', {
+				+ ' 在萌娘百科[https://zh.moegirl.org/User_talk:' + owner_name
+				+ ' 發了一則新留言] ，煩請撥空前往查看。 --~~~~', {
 			section : 'new',
 			sectiontitle : '您在moegirl有一則新留言',
 			summary : 'cross-wiki 新留言提醒',
 			nocreate : 1,
 			bot : 1
 		}, function() {
-			wiki.page('User_talk:' + owner_name).edit(function(page_data) {
+			wiki.page('User talk:' + owner_name).edit(function(page_data) {
 				var
 				/**
 				 * {String}page content, maybe undefined. 條目/頁面內容 =
@@ -71,7 +72,7 @@ wiki.page('User_talk:' + owner_name, function(page_data) {
 				 */
 				content = CeL.wiki.content_of(page_data);
 				if (content) {
-					return content.trim() + '\n: 已提醒。 --~~~~';
+					return content.trim() + '\n: 已提醒 [[zhwiki:User talk:' + owner_name + ']]。 --~~~~';
 				}
 			}, {
 				summary : '已作 cross-wiki 留言提醒',
