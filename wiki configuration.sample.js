8614beaf49c3af4f251a47e908105ea57e77bcca
kanasimi <kanasimi@gmail.com>
2017-04-02 07:27:58 +0800

    +wiki configuration.js

---
+++ wiki configuration.sample.js
@@ -0,0 +1,12 @@
+﻿/**
+ * @name CeJS wiki configuration for node.js.
+ * @fileoverview ** This file is private.
+ */
+
+'use strict';
+
+var _global = Function('return this')();
+
+_global.owner_name = 'owner_name';
+_global.user_name = 'user_name';
+_global.user_password = 'user_password';
