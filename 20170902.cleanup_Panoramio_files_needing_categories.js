a6674f5c57c6c9b9783c73569ef896ffc4c5be2a
kanasimi <kanasimi@gmail.com>
2017-10-02 06:20:22 +0800

    20170902.cleanup_Panoramio_files_needing_categories.js: 完成。正式運用。

--- 20170902.cleanup_Panoramio_files_needing_categories.js
+++ 20170902.cleanup_Panoramio_files_needing_categories.js
@@ -3,8 +3,8 @@
 Cleanup Panoramio files needing categories
 https://commons.wikimedia.org/wiki/Commons:Bots/Requests/Cewbot_4
 
- 2017/9/2 16:4:57	初版試營運。
- 	完成。正式運用。
+2017/9/2 16:4:57	初版試營運。
+2017/10/1 20:46:50	完成。正式運用。
 
  */
 
@@ -28,30 +28,39 @@ var meaningful_categories = CeL.null_Object(),
 page_options = {
 	rvprop : 'timestamp|content|user|flags'
 },
+// [[Commons:Bots/Requests/Cewbot 4|bot test]]:
+// Remove "needing categories" category which is already including category.
+default_summary = 'bot: Remove "needing categories" which has ',
 //
 edit_options = {
 	bot : 1,
 	minor : 1,
-	nocreate : 1,
-	summary : '[[Commons:Bots/Requests/Cewbot 4|bot test]]: Remove "needing categories" category which is already including category.'
+	nocreate : 1
 };
 
-get_files_of_category('Photos from Panoramio needing categories by date',
-		for_file);
+get_files_of_category('Photos from Panoramio needing categories by date');
 
-function get_files_of_category(category, for_file) {
+function get_files_of_category(category, callback) {
 	wiki.categorymembers(category, function(list) {
-		list.forEach(function(page) {
+		CeL.debug('All ' + list.length + ' pages get from '
+		//
+		+ CeL.wiki.title_link_of(category), 1, 'get_files_of_category');
+		list.run_async(function(run_next, page) {
 			if (page.ns === CeL.wiki.namespace.hash.category) {
 				// Search all sub-categories.
-				get_files_of_category(page, for_file);
+				get_files_of_category(page, run_next);
 			} else if (page.ns === CeL.wiki.namespace.hash.file) {
-				wiki.page(page, page_options).edit(for_file, edit_options);
+				CeL.debug('處理頁面 ' + CeL.wiki.title_link_of(page), 1,
+						'get_files_of_category');
+				wiki.page(page, for_file.bind({
+					run_next : run_next
+				}), page_options);
 			} else {
 				CeL.log('Skip ' + CeL.wiki.title_link_of(page));
 			}
-		});
+		}, callback);
 	}, {
+		// limit : 5
 		limit : 'max'
 	});
 }
@@ -65,6 +74,8 @@ function for_file(page_data, error) {
 	content = CeL.wiki.content_of(page_data);
 
 	if (!content) {
+		this.run_next();
+		return;
 		return [ CeL.wiki.edit.cancel,
 		//
 		'No contents: [[' + title + ']]! 沒有頁面內容！' ];
@@ -73,6 +84,8 @@ function for_file(page_data, error) {
 	var revision = CeL.wiki.content_of.revision(page_data), user = revision.user;
 	if (('bot' in revision) || /bot/i.test(user)) {
 		// Untouched. e.g., 'Panoramio upload bot'
+		this.run_next();
+		return;
 		return [ CeL.wiki.edit.cancel, 'skip' ];
 	}
 
@@ -105,13 +118,22 @@ function for_file(page_data, error) {
 	});
 
 	if (needing_categories.length !== 1) {
-		throw title + ': needing_categories.length !== 1';
+		// e.g., [[File:Eerbeek - panoramio.jpg]]
+		CeL.debug(CeL.wiki.title_link_of(page_data)
+				+ ': needing_categories.length === '
+				+ needing_categories.length + ' !== 1');
 	}
 
 	function last_check_and_return() {
 		if (categories.length === 0) {
 			return [ CeL.wiki.edit.cancel, 'skip' ];
 		}
+
+		this.summary = default_summary
+		//
+		+ categories.map(function(category) {
+			return category.toString();
+		}).join(', ');
 
 		if (false) {
 			CeL.info(CeL.wiki.title_link_of(page_data) + ': ' + user);
@@ -138,22 +160,23 @@ function for_file(page_data, error) {
 		return parser.toString();
 	}
 
-	if (categories_to_check.length === 0) {
-		return last_check_and_return();
+	if (categories_to_check.length > 0) {
+		CeL.info('Checking categories: ' + categories_to_check);
+		// Checking each category to exclude __HIDDENCAT__ categories.
+		categories_to_check.forEach(function(category) {
+			wiki.page('Category:' + category.name, function(category_page) {
+				var meaningful = CeL.wiki.content_of(category_page);
+				meaningful = !meaningful
+						|| !meaningful.includes('__HIDDENCAT__');
+				// set cache
+				meaningful_categories[category.name] = meaningful;
+				if (meaningful) {
+					categories.push(category);
+				}
+			});
+		});
 	}
 
-	// Checking each category to exclude __HIDDENCAT__ categories.
-	categories_to_check.forEach(function(category) {
-		wiki.page(category.name, function(category_page) {
-			var meaningful = CeL.wiki.content_of(category_page);
-			meaningful = !meaningful || !meaningful.includes('__HIDDENCAT__');
-			// set cache
-			meaningful_categories[category.name] = meaningful;
-			if (meaningful) {
-				categories.push(category);
-			}
-		});
-	});
-	wiki.page(page_data).edit(last_check_and_return);
-	return [ CeL.wiki.edit.cancel, 'skip' ];
+	wiki.page(page_data).edit(last_check_and_return, edit_options).run(
+			this.run_next);
 }
