c37bb989e8b1d8424b61804014a3a39ea13aeb0c
kanasimi <kanasimi@gmail.com>
2018-01-26 05:11:56 +0800

    use item_count_pairs

--- 20180125.most_sitelinked_articles_to_write.js
+++ 20180125.most_sitelinked_articles_to_write.js
@@ -19,7 +19,7 @@ var
 /** {Object}wiki operator 操作子. */
 wiki = Wiki(true),
 //
-MIN_COUNT = 20, reget = true,
+MIN_COUNT = 50, reget = true,
 //
 most_sitelinked_items_filename = 'most_sitelinked_items.json';
 
@@ -38,7 +38,7 @@ SQL_session = new CeL.wiki.SQL(function(error) {
 	}
 }, 'wikidata');
 
-get_most_sitelinked_items(function(items_of_count, item_count_of) {
+get_most_sitelinked_items(function(items_of_count, item_count_pairs) {
 	get_most_sitelinked_items_exclude_language('zh', function(items_of_count,
 			item_list) {
 		get_most_sitelinked_items_exclude_language('ja', function(
@@ -54,11 +54,11 @@ get_most_sitelinked_items(function(items_of_count, item_count_of) {
 			CeL.info('Done.');
 		}, {
 			reget : reget,
-			item_count_of : item_count_of
+			item_count_pairs : item_count_pairs
 		});
 	}, {
 		reget : reget,
-		item_count_of : item_count_of
+		item_count_pairs : item_count_pairs
 	});
 }, {
 	reget : reget
@@ -73,12 +73,13 @@ function get_most_sitelinked_items(callback, options) {
 			throw error;
 		}
 
-		CeL.info('All ' + rows.length + ' items.');
-		var items_of_count = CeL.null_Object(), item_count_of = [];
+		CeL.info('All ' + rows.length + ' items more than ' + MIN_COUNT
+				+ ' sitelinks.');
+		var items_of_count = CeL.null_Object(), item_count_pairs = [];
 		rows.forEach(function(row) {
 			var item_id = row.ips_item_id, link_count = row.link_count;
-			// item_count_of[item_id] = link_count
-			item_count_of[item_id] = link_count;
+			// item_count_pairs = [ [item_id,link_count], ... ]
+			item_count_pairs.push([ item_id, link_count ]);
 			// items_of_count[link_count] = [ item_id, item_id, item_id, ... ]
 			if (items_of_count[link_count]) {
 				items_of_count[link_count].push(item_id);
@@ -88,8 +89,8 @@ function get_most_sitelinked_items(callback, options) {
 		});
 
 		CeL.write_file(data_filename, JSON.stringify([ items_of_count,
-				item_count_of ]));
-		callback(items_of_count, item_count_of);
+				item_count_pairs ]));
+		callback(items_of_count, item_count_pairs);
 	}
 
 	options = CeL.setup_options(options);
@@ -108,7 +109,7 @@ function get_most_sitelinked_items(callback, options) {
 	// HAVING SUM(CASE WHEN ips_site_id = "jawiki" THEN 1 ELSE 0 END) = 0
 	// ORDER BY `link_count` DESC
 	// 之類的方法，將大大增加查詢的時間。
-	SQL_get_sitelink_count = 'SELECT ips_item_id, COUNT(*) AS `link_count` FROM wb_items_per_site GROUP BY ips_item_id HAVING `link_count` > '
+	SQL_get_sitelink_count = 'SELECT ips_item_id, COUNT(*) AS `link_count` FROM wb_items_per_site GROUP BY ips_item_id HAVING `link_count` >= '
 			+ MIN_COUNT + ' ORDER BY ips_item_id';
 
 	CeL.info('get_most_sitelinked_items: Run SQL: ' + SQL_get_sitelink_count);
@@ -122,25 +123,31 @@ function get_most_sitelinked_items_exclude_language(language, callback, options)
 			throw error;
 		}
 
-		CeL.info(language + ': ' + rows.length + ' items. Filtering items...');
-		var items_of_count = CeL.null_Object(), item_hash = CeL.null_Object(), item_list = [];
-		rows.forEach(function(row) {
-			// row: 在language這個語言中有的項目
-			item_hash[row.ips_item_id] = undefined;
-		});
-		var item_count_of = options.item_count_of;
-		for ( var item_id in item_count_of) {
-			if (item_id in item_hash) {
+		// row: 在 language 這個語言中有的項目。
+		CeL.info(language + ': ' + rows.length + ' items more than '
+				+ MIN_COUNT + ' sitelinks. Filtering items...');
+		var items_of_count = CeL.null_Object(), item_list = [], index_of_rows = 0, id_of_current_row = rows[index_of_rows].ips_item_id;
+		// 示意範例: rows = [ 1,3,5 ]
+		// item_count_pairs = [ [1,1], [2,1], [3,1], [4,1], [5,1], ]
+		options.item_count_pairs.forEach(function(pair) {
+			var item_id = pair[0];
+			if (item_id === id_of_current_row) {
+				id_of_current_row = rows[++index_of_rows]
+						&& rows[index_of_rows].ips_item_id;
 				return;
 			}
+
 			item_list.push(item_id);
-			var link_count = item_count_of[item_id];
+			var link_count = pair[1];
 			// items_of_count[link_count] = [ item_id, item_id, item_id, ... ]
 			if (link_count in items_of_count) {
 				items_of_count[link_count].push(item_id);
 			} else {
 				items_of_count[link_count] = [ item_id ];
 			}
+		});
+		if (id_of_current_row) {
+			throw '當前 rows[' + index_of_rows + '] 還有資訊尚未處理!';
 		}
 
 		CeL.write_file(data_filename, JSON.stringify([ items_of_count,
@@ -169,7 +176,7 @@ function get_most_sitelinked_items_exclude_language(language, callback, options)
 			+ language
 			+ 'wiki"';
 
-	SQL_get_sitelink_count = 'SELECT ips_item_id FROM wb_items_per_site WHERE ips_item_id IN (SELECT ips_item_id FROM wb_items_per_site GROUP BY ips_item_id HAVING COUNT(*) > '
+	SQL_get_sitelink_count = 'SELECT ips_item_id FROM wb_items_per_site WHERE ips_item_id IN (SELECT ips_item_id FROM wb_items_per_site GROUP BY ips_item_id HAVING COUNT(*) >= '
 			+ MIN_COUNT
 			+ ') AND ips_site_id = "'
 			+ language
