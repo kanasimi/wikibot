4e2e1d6e74c11b91439811c829c263c12bb846d8
kanasimi <kanasimi@gmail.com>
2019-07-14 07:24:20 +0800

    add 暂存

--- 20170515.signature_check.js
+++ 20170515.signature_check.js
@@ -130,7 +130,7 @@ PATTERN_archive = /{{ *(?:(?:Talk ?)?archive|存檔|(?:讨论页)?存档|Aan|来
 // https://commons.wikimedia.org/wiki/Commons:GlobalReplace
 // "!nosign!": 已經參考、納入了一部分 [[commons:User:SignBot|]] 的做法。
 // @see [[Wikipedia:Twinkle]] ([[WP:TW]])
-PATTERN_revert_or_bot_summary = /还原|還原|revert|回退|撤銷|撤销|取消.*(编辑|編輯)|更改回|維護|暫存|臨時保存|替换引用|!nosign!|!nobot!|AutoWikiBrowser|自動維基瀏覽器|自动维基浏览器|GlobalReplace/i,
+PATTERN_revert_or_bot_summary = /还原|還原|revert|回退|撤銷|撤销|取消.*(编辑|編輯)|更改回|維護|暫存|暂存|臨時保存|替换引用|!nosign!|!nobot!|AutoWikiBrowser|自動維基瀏覽器|自动维基浏览器|GlobalReplace/i,
 // 可以在頁面中加入 "{{NoAutosign}}" 來避免這個任務於此頁面添加簽名標記。
 // 請機器人注意: 本頁面不採用補簽名
 PATTERN_ignore = /本頁面不.{0,3}補簽名/,
