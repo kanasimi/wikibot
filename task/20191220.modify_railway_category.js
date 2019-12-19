3f34fbe6566ec085c06d64f4f7230d06a7483f7f
kanasimi <kanasimi@gmail.com>
2019-12-20 06:42:47 +0800

    + pages_need_to_modify

--- task/20191220.modify_railway_category.js
+++ task/20191220.modify_railway_category.js
@@ -17,14 +17,18 @@ use_language = 'zh';
 
 // ----------------------------------------------------------------------------
 
-
+const pages_need_to_modify = [];
 
 async function modify_root_category(category) {
-	const category_tree = await wiki.category_tree(category, {
-		filter: /(?:铁路|线‎)‎$/,
+	await wiki.category_tree(category, {
+		category_filter: (page_data) => /(?:铁路)$/.test(page_data.title),
+		page_filter: (page_data) => /(?:铁路|线)$/.test(page_data.title),
+		for_each_page: (page_data) => pages_need_to_modify.push(page_data),
 		depth: 3
 	});
-	console.log(category_tree.subcategories.北京市铁路);
+	// console.log(category_tree);
+	// console.log(category_tree[KEY_subcategories].北京市铁路);
+	console.log(pages_need_to_modify);
 }
 
 // ----------------------------------------------------------------------------
@@ -32,7 +36,7 @@ async function modify_root_category(category) {
 
 async function main_process() {
 	await modify_root_category('Category:中国各省铁路');
-	//await modify_root_category('Category:中国高速铁路线');
+	// await modify_root_category('Category:中国高速铁路线');
 
 	CeL.info((new Date).format() + '	Done.');
 }
