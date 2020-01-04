a26aea16ccfa943bee32f2e79024c5e30e191a2e
kanasimi <kanasimi@gmail.com>
2020-01-05 06:03:54 +0800

    + ブランドとしてのXboxの記事作成に伴うリンク修正

---
+++ task/20200101.ブランドとしてのXboxの記事作成に伴うリンク修正.js
@@ -0,0 +1,26 @@
+﻿/*
+
+2020/1/1 15:49:56	初版試營運
+
+ */
+
+'use strict';
+
+// Load replace tools.
+const replace_tool = require('./replace_tool.js');
+//import { replace } from './replace_tool.js';
+
+// ----------------------------------------------------------------------------
+
+//async function main_process()
+(async () => {
+	await replace_tool.replace({
+		language: 'ja',
+		//summary: '',
+		// 'small_oldid/big_new_diff' or {Number}new
+		diff_id: '75554468/75573475',
+		section_title: 'ブランドとしてのXboxの記事作成に伴うリンク修正'
+	}, {
+		'Xbox': 'Xbox (ゲーム機)'
+	});
+})();
