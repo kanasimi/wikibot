2fe4d6504ac88b8b69a5a54e2ee315f2643321b8
kanasimi <kanasimi@gmail.com>
2020-01-22 11:46:50 +0800

    + 20200122.update_vital_articles.js

---
+++ routine/20200122.update_vital_articles.js
@@ -0,0 +1,37 @@
+﻿/*
+
+	初版試營運
+
+
+ */
+
+'use strict';
+
+// Load CeJS library and modules.
+require('../wiki loader.js');
+
+CeL.run('application.net.wiki.featured_content');
+
+/** {Object}wiki operator 操作子. */
+const wiki = new Wikiapi;
+// globalThis.use_language = 'zh';
+use_language = 'en';
+
+// ----------------------------------------------------------------------------
+
+prepare_directory(base_directory);
+
+(async () => {
+	await wiki.login(user_name, user_password, use_language);
+	// await wiki.login(null, null, use_language);
+	await main_process();
+})();
+
+async function main_process() {
+	await wiki.get_featured_content();
+	//console.log(wiki.FC_data_hash);
+	const vital_articles_classes = await wiki.categorymembers('Wikipedia vital articles by class');
+}
+
+// ----------------------------------------------------------------------------
+
