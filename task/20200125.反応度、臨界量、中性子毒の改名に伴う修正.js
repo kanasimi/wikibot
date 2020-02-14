c649b39985b70b04f47901792cf70783d6cb6299
kanasimi <kanasimi@gmail.com>
2020-02-15 04:38:03 +0800

    + 20200125.反応度、臨界量、中性子毒の改名に伴う修正.js

---
+++ task/20200125.反応度、臨界量、中性子毒の改名に伴う修正.js
@@ -0,0 +1,28 @@
+﻿/*
+
+2020/1/25 9:16:13	初版試營運
+
+ */
+
+'use strict';
+
+// Load replace tools.
+const replace_tool = require('./replace_tool.js');
+//import { replace } from './replace_tool.js';
+
+// ----------------------------------------------------------------------------
+
+//async function main_process()
+(async () => {
+	await replace_tool.replace({
+		language: 'ja',
+		//summary: '',
+		// 'small_oldid/big_new_diff' or {Number}new
+		diff_id: '75666797/75713740',
+		section_title: '反応度、臨界量、中性子毒の改名に伴う修正'
+	}, {
+		'反応度 (原子力)': '反応度',
+		'臨界量 (原子力)': '臨界量',
+		'毒物質 (原子力)': '中性子毒',
+	});
+})();
