85f6f2e9028d808aa87a7bb88d98be2c900988fd
kanasimi <kanasimi@gmail.com>
2020-04-08 05:42:05 +0800

    + replace/*

---
+++ replace/20200401.首都大学東京関連2 (Category、Template修正) .js
@@ -0,0 +1,31 @@
+﻿/*
+
+2020/4/1 8:7:34	初版試營運
+
+ */
+
+'use strict';
+
+// Load replace tools.
+const replace_tool = require('./replace_tool.js');
+//import { replace } from './replace_tool.js';
+
+// ----------------------------------------------------------------------------
+
+//async function main_process()
+(async () => {
+	await replace_tool.replace({
+		language: 'ja',
+		//summary: '',
+		// 'small_oldid/big_new_diff' or {Number}new
+		diff_id: 76853014,
+		section_title: '首都大学東京関連2 (Category、Template修正) '
+	}, {
+		'Category:首都大学東京': 'Category:東京都立大学',
+		'Category:首都大学東京学長': 'Category:東京都立大学学長',
+		'Category:首都大学東京の人物': 'Category:東京都立大学の人物',
+		'Template:首都大学東京': 'Template:東京都立大学',
+		'Template:首都大学東京の前身諸機関': 'Template:東京都立大学の前身諸機関',
+		'Category:首都大学東京出身の人物': 'Category:東京都立大学出身の人物',
+	});
+})();
---
+++ replace/20200401.首都大学東京関連3(Category).js
@@ -0,0 +1,31 @@
+﻿/*
+
+2020/4/3 5:10:50	初版試營運
+
+ */
+
+'use strict';
+
+// Load replace tools.
+const replace_tool = require('./replace_tool.js');
+//import { replace } from './replace_tool.js';
+
+// ----------------------------------------------------------------------------
+
+//async function main_process()
+(async () => {
+	await replace_tool.replace({
+		language: 'ja',
+
+		// 可省略 `diff_id` 的條件: 以新章節增加請求，且編輯摘要包含 `/* section_title */`
+		// 'small_oldid/big_new_diff' or {Number}new
+		diff_id: 76853014,
+
+		// 可省略 `section_title` 的條件: 檔案名稱即 section_title
+		//section_title: '',
+
+		//summary: '',
+	}, {
+		'Category:首都大学東京の教員': 'Category:東京都立大学の教員',
+	});
+})();
