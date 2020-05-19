fd0617039eb5db653d36f87e28e709f731574c7b
kanasimi <kanasimi@gmail.com>
2020-05-20 06:25:15 +0800

    fix function for_log_page(page_data)

--- archive_logs.js
+++ archive_logs.js
@@ -235,7 +235,7 @@ function for_log_page(page_data) {
 			// 即使沒有內容，只要存在頁面就當作可以寫入。
 			|| (typeof log_page === 'string' ?
 			// 頁面大小系統上限 2,048 KB = 2 MB。
-			log_page.length + log_size < 2e6 : !config.nocreate)) {
+			log_page.length + log_size <= Math.pow(8, 7) : !config.nocreate)) {
 				return "'''{{font color|#54f|#ff6|存檔長度" + log_size
 				// TODO: internationalization
 				+ "字元。}}'''\n" + content.slice(matched.index).trim();
