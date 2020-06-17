072053680d2b411f350307b2df37c678797ec005
kanasimi <kanasimi@gmail.com>
2020-06-18 05:51:10 +0800

    + 20200618.鄧小平への記事名変更に伴う変更.js

---
+++ replace/20200618.鄧小平への記事名変更に伴う変更.js
@@ -0,0 +1,17 @@
+﻿/*
+
+2020/6/18 5:6:15	初版試營運
+
+ */
+
+'use strict';
+
+// Load replace tools.
+const replace_tool = require('./replace_tool.js');
+
+replace_tool.replace({
+	language: 'ja',
+	diff_id: 78059250,
+}, {
+	'トウ小平': REDIRECT_TARGET,
+});
