624d4fca7b3353a882e5fe907167b878ca0ecf78
kanasimi <kanasimi@gmail.com>
2020-07-24 07:57:06 +0800

    Specify page title to prevent redirecting to important page.

--- 20160719.clean_sandbox.js
+++ 20160719.clean_sandbox.js
@@ -23,14 +23,71 @@ min_interval = '30m', JD = CeL.date.Julian_day(new Date);
 
 // --------------------------------------------------------
 
-function clean_wiki(wiki, replace_to, _summary, page) {
+function clean_wiki_sandbox(wiki, replace_to, _summary, page) {
 	if (!CeL.wiki.is_wiki_API(wiki)) {
 		/** {Object}wiki operator 操作子. */
 		wiki = Wiki(true, wiki);
 	}
 
-	wiki.redirect_to(page || 'Project:Sandbox', function(redirect_data,
-			page_data) {
+	var edit_options = {
+		summary : _summary || summary,
+		nocreate : 1,
+		bot : 1
+	};
+
+	function edit_sandbox(page_data) {
+		// 在執行清理工作前，先行檢查用戶最後編輯時間。
+		var time_diff = Date.now()
+				- Date.parse(page_data.revisions[0].timestamp);
+		if (time_diff < CeL.date.to_millisecond(min_interval)) {
+			return [ CeL.wiki.edit.cancel,
+			// 這一次沒清理到的話，會等到下一次再清理。
+			'用戶最後編輯時間短於' + min_interval + '，跳過清理。' ];
+		}
+
+		var
+		/**
+		 * {String}page content, maybe undefined. 條目/頁面內容 =
+		 * CeL.wiki.revision_content(revision)
+		 */
+		content = CeL.wiki.content_of(page_data);
+
+		// 運作原理: 在清除前後空白之後，若是與預設的文字相同，就不會更動。
+		if (replace_to.trim() === content.trim()
+		// 但也不能太過份。
+		&& !/[\n\s]{3}$/.test(content) && !/^[\n\s]{3}/.test(content)) {
+			return [ CeL.wiki.edit.cancel, 'skip' ];
+		}
+
+		/**
+		 * <code>
+		var PATTERN = /\n==[^=]+==([\n\s]*(?:<[^<>]+>)?)\n?$/;
+		'header</noinclude>'.trim() === 'header\n== 請在這行文字底下進行您的測試 ==</noinclude>\n'.replace(PATTERN, '$1').trim()
+		'header'.trim() === 'header\n== 請在這行文字底下進行您的測試 ==\n'.replace(PATTERN, '$1').trim()
+		'header\n</noinclude>'.trim() === 'header\n== 請在這行文字底下進行您的測試 ==\n</noinclude>\n'.replace(PATTERN, '$1').trim()
+		'header\n'.trim() === 'header\n== 請在這行文字底下進行您的測試 ==\n'.replace(PATTERN, '$1').trim()
+		</code>
+		 */
+		if (CeL.wiki.site_name(wiki) === 'zhwiki'
+		// 為 Jimmy-bot 特設, 避免與 Jimmy-bot 編輯戰. e.g., [[Template:沙盒]]
+		&& replace_to.replace(
+		//
+		/\n==[^=]+==([\n\s]*(?:<[^<>]+>)?)\n?$/, '$1').trim()
+		//
+		=== content.trim()) {
+			return [ CeL.wiki.edit.cancel, 'skip' ];
+		}
+
+		return replace_to;
+	}
+
+	if (page) {
+		// If page title spicified, do not redirect.
+		wiki.page(page).edit(edit_sandbox, edit_options);
+		return;
+	}
+
+	wiki.redirect_to('Project:Sandbox', function(redirect_data, page_data) {
 		// console.log(page_data.response.query);
 		// console.log(redirect_data);
 
@@ -43,64 +100,18 @@ function clean_wiki(wiki, replace_to, _summary, page) {
 						+ ' Incase the public sandbox being moved.'
 			});
 		}
-		wiki.edit(function(page_data) {
-			// 在執行清理工作前，先行檢查用戶最後編輯時間。
-			var time_diff = Date.now()
-					- Date.parse(page_data.revisions[0].timestamp);
-			if (time_diff < CeL.date.to_millisecond(min_interval)) {
-				return [ CeL.wiki.edit.cancel,
-				// 這一次沒清理到的話，會等到下一次再清理。
-				'用戶最後編輯時間短於' + min_interval + '，跳過清理。' ];
-			}
-
-			var
-			/**
-			 * {String}page content, maybe undefined. 條目/頁面內容 =
-			 * CeL.wiki.revision_content(revision)
-			 */
-			content = CeL.wiki.content_of(page_data);
-
-			// 運作原理: 在清除前後空白之後，若是與預設的文字相同，就不會更動。
-			if (replace_to.trim() === content.trim()
-			// 但也不能太過份。
-			&& !/[\n\s]{3}$/.test(content) && !/^[\n\s]{3}/.test(content)) {
-				return [ CeL.wiki.edit.cancel, 'skip' ];
-			}
-
-			/**
-			 * <code>
-			var PATTERN = /\n==[^=]+==([\n\s]*(?:<[^<>]+>)?)\n?$/;
-			'header</noinclude>'.trim() === 'header\n== 請在這行文字底下進行您的測試 ==</noinclude>\n'.replace(PATTERN, '$1').trim()
-			'header'.trim() === 'header\n== 請在這行文字底下進行您的測試 ==\n'.replace(PATTERN, '$1').trim()
-			'header\n</noinclude>'.trim() === 'header\n== 請在這行文字底下進行您的測試 ==\n</noinclude>\n'.replace(PATTERN, '$1').trim()
-			'header\n'.trim() === 'header\n== 請在這行文字底下進行您的測試 ==\n'.replace(PATTERN, '$1').trim()
-			</code>
-			 */
-			if (CeL.wiki.site_name(wiki) === 'zhwiki'
-			// 為 Jimmy-bot 特設, 避免與 Jimmy-bot 編輯戰. e.g., [[Template:沙盒]]
-			&& replace_to.replace(
-			//
-			/\n==[^=]+==([\n\s]*(?:<[^<>]+>)?)\n?$/, '$1').trim()
-			//
-			=== content.trim()) {
-				return [ CeL.wiki.edit.cancel, 'skip' ];
-			}
-
-			return replace_to;
-		}, {
-			summary : _summary || summary,
-			nocreate : 1,
-			bot : 1
-		});
+		wiki.edit(edit_sandbox, edit_options);
 	});
 }
 
 // --------------------------------------------------------
 
-clean_wiki(
+clean_wiki_sandbox(
 		'test',
 		'<noinclude>{{Sandbox}}</noinclude>\n== Please start your testing below this line ==\n',
-		'Clearing the sandbox. If you want to keep a longer time, please tasting in the [[Special:MyPage/Sandbox|personal sandbox]], and you may want to check the revision history of the sandbox as well.');
+		'Clearing the sandbox. If you want to keep a longer time, please tasting in the [[Special:MyPage/Sandbox|personal sandbox]], and you may want to check the revision history of the sandbox as well.',
+		// Specify page title to prevent redirecting to important page.
+		'Wikipedia:Sandbox');
 
 // --------------------------
 
@@ -108,50 +119,53 @@ var zhwiki = Wiki(true, 'zh'),
 // <!-- 請注意：請不要變更這行文字以及這行文字以上的部份！ -->\n\n
 // ここから下に書き込んでください。
 zhwiki_announcement = '{{請注意：請在這行文字底下進行您的測試，請不要刪除或變更這行文字以及這行文字以上的部份。}}\n{{请注意：请在这行文字底下进行您的测试，请不要删除或变更这行文字以及这行文字以上的部分。}}\n';
-clean_wiki(zhwiki, zhwiki_announcement// + '== 請在這行文字底下進行您的測試 ==\n'
+clean_wiki_sandbox(zhwiki, zhwiki_announcement// + '== 請在這行文字底下進行您的測試 ==\n'
 );
 
 // 預防改完以後又被 Jimmy-bot 改過來，現在採用相同的格式。
 
 // @see [[Special:链入页面/Template:Sandbox]]
 // TODO: [[模块:沙盒]], [[File:沙盒.png]]
-clean_wiki(zhwiki, zhwiki_announcement// + '== 請在這行文字底下進行您的測試 ==\n'
+clean_wiki_sandbox(zhwiki, zhwiki_announcement// + '== 請在這行文字底下進行您的測試 ==\n'
 , null, 'Wikipedia:使用指南 (编辑)/沙盒');
-clean_wiki(zhwiki, zhwiki_announcement// + '== 請在這行文字底下進行您的測試 ==\n'
+clean_wiki_sandbox(zhwiki, zhwiki_announcement// + '== 請在這行文字底下進行您的測試 ==\n'
 , null, 'Draft:沙盒');
-clean_wiki(zhwiki, zhwiki_announcement// + '== 請在這行文字底下進行您的測試 ==\n'
+clean_wiki_sandbox(zhwiki, zhwiki_announcement// + '== 請在這行文字底下進行您的測試 ==\n'
 , null, 'Category:Foo');
-clean_wiki(zhwiki, '<noinclude>' + zhwiki_announcement.trim()
+clean_wiki_sandbox(zhwiki, '<noinclude>' + zhwiki_announcement.trim()
 // + '== 請在這行文字底下進行您的測試 =='
 + '</noinclude>\n', null, 'Template:沙盒');
-clean_wiki(zhwiki, zhwiki_announcement + '{{S/wnote}}\n'
+clean_wiki_sandbox(zhwiki, zhwiki_announcement + '{{S/wnote}}\n'
 // + '== 請在這行文字底下進行您的測試 ==\n'
 , null, 'User talk:Sandbox for user warnings~zhwiki');
 
 // --------------------------
 
-clean_wiki('wikinews',
-		'<noinclude>{{Sandbox}}</noinclude>\n== 請在這行文字底下進行您的測試 ==\n');
+clean_wiki_sandbox('wikinews',
+		'<noinclude>{{Sandbox}}</noinclude>\n== 請在這行文字底下進行您的測試 ==\n',
+		undefined,
+		// 對於機器人有管理員權限的wiki，必須準確設定頁面名稱，預防有人將頁面導向到主頁之類重要頁面。
+		'Wikinews:沙盒');
 
 // --------------------------
 
 // 由於維基文庫參與人數太少，沙盒清理可以放寬期限，例如每週一次。
 if (force || JD % 7 === 0) {
-	clean_wiki('wikisource',
+	clean_wiki_sandbox('wikisource',
 			'<noinclude>{{Sandbox}}</noinclude>\n== 請在這行文字底下進行您的測試 ==\n', null,
 			'Wikisource:沙盒');
 }
 
 // --------------------------
 
-clean_wiki('zh-classical',
+clean_wiki_sandbox('zh-classical',
 		'<noinclude>{{Sandbox}}</noinclude>\n== 請於此行文下習纂而莫去本行以上文 ==\n');
 
 // --------------------------
 
 // 兩天一次指的是兩天檢查一次。...了解，應該可以搬運過來了。
 if (force || JD % 2 === 0) {
-	clean_wiki('wikiversity',
+	clean_wiki_sandbox('wikiversity',
 	// 請勿刪除此行
 	'<noinclude>{{Sandbox}}</noinclude>\n== 請在這行文字底下進行您的測試 ==\n', null,
 			'Wikiversity:沙盒');
