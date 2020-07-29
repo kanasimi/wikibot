21df86fdff1b9880118fab44b922ebe32c375f09
kanasimi <kanasimi@gmail.com>
2020-07-30 07:06:40 +0800

    + 20200728.「コンスタンティノス11世」→「コンスタンティノス11世パレオロゴス」の改名に伴うリンク修正依頼.js

---
+++ replace/20200728.「コンスタンティノス11世」→「コンスタンティノス11世パレオロゴス」の改名に伴うリンク修正依頼.js
@@ -0,0 +1,8 @@
+﻿'use strict';
+
+// Load replace tools.
+const replace_tool = require('./replace_tool.js');
+
+replace_tool.replace(null, {
+	'コンスタンティノス11世パレオロゴス|コンスタンティノス11世': 'コンスタンティノス11世パレオロゴス',
+});
