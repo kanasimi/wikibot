e8e272481645410e3ad34320749c668ad324e873
kanasimi <kanasimi@gmail.com>
2020-09-05 16:11:28 +0800

    fix async function modify_pages()

--- routine/20191214.maintain_historical_deletion_records.js
+++ routine/20191214.maintain_historical_deletion_records.js
@@ -650,7 +650,7 @@ async function check_deletion_page(JDN, page_data) {
 // ----------------------------------------------------------------------------
 
 async function modify_pages() {
-	for (const [_page_title, discussions] of Object.entries(pages_to_modify)) {
+	for (let [_page_title, discussions] of Object.entries(pages_to_modify)) {
 		const page_title = wiki.to_talk_page(_page_title);
 		if (!page_title)
 			continue;
