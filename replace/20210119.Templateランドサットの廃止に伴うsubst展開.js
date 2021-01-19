51149de1e80dafda2b7b2c07053c41cb43d4135b
kanasimi <kanasimi@gmail.com>
2021-01-20 06:39:05 +0800

    + replace/20210119.Templateランドサットの廃止に伴うsubst展開.js

---
+++ replace/20210119.Templateランドサットの廃止に伴うsubst展開.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+
+// Load replace tools.
+const replace_tool = require('./replace_tool.js');
+
+// ----------------------------------------------------------------------------
+
+replace_tool.replace(null, {
+	'Template:ランドサット': {
+		namespace: '*',
+		for_template(token, index, parent) {
+			token[0] = 'subst:' + token[0];
+		},
+	},
+});
