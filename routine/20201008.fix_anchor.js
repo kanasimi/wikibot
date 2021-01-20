f48e4107cfc3577b4882805d5d109f6c4b7be0da
kanasimi <kanasimi@gmail.com>
2021-01-20 14:12:28 +0800

    警告: index 以 "|" 終結會被視為 patten 明確終結，並且 "|" 將被吃掉。

--- routine/20201008.fix_anchor.js
+++ routine/20201008.fix_anchor.js
@@ -771,7 +771,9 @@ async function check_page(target_page_data, options) {
 		if (anchor_token) {
 			wikitext_to_add = `\n* <nowiki>${anchor_token}</nowiki>${record
 				// ，且現在失效中<syntaxhighlight lang="json">...</syntaxhighlight>
-				? `${record.disappear ? ' ' + CeL.gettext('此網頁錨點[[Special:Diff/%1|曾被刪除過]]。', record.disappear.revid) : ''
+				? `${record.disappear ? ' '
+					// 警告: index 以 "|" 終結會被視為 patten 明確終結，並且 "|" 將被吃掉。
+					+ CeL.gettext('此網頁錨點[[Special:Diff/%1||曾被刪除過]]。', record.disappear.revid) : ''
 				// ，且現在失效中<syntaxhighlight lang="json">...</syntaxhighlight>
 				} <!-- ${JSON.stringify(record)} -->` : ''}`;
 			CeL.error(`${add_note_for_broken_anchors.name}: ${CeL.wiki.title_link_of(talk_page_title)}: ${CeL.gettext('提醒失效的網頁錨點')}: ${CeL.wiki.title_link_of(talk_page_title)}`);
