a1a096444210afc3e986d5a622693c4f54399b0c
kanasimi <kanasimi@gmail.com>
2021-04-16 04:28:40 +0800

    20210414.翻訳用出典テンプレートのsubst展開.js : a work version

--- replace/20210414.翻訳用出典テンプレートのsubst展開.js
+++ replace/20210414.翻訳用出典テンプレートのsubst展開.js
@@ -3,28 +3,60 @@
 // Load replace tools.
 const replace_tool = require('./replace_tool.js');
 
-replace_tool.replace(null, {
-	'Template:Cite web/German': { namespace: '0', for_template },
-	'Template:Cite web/French': { namespace: '0', for_template },
-	'Template:Cite web/Swedish': { namespace: '0', for_template },
-	'Template:Cita libro': { namespace: '0', for_template },
-	'Template:Книга': { namespace: '0', for_template },
-	'Template:Статья': { namespace: '0', for_template },
-	'Template:Literatur': { namespace: '0', for_template },
-});
+const task_configuration = Object.create(null);
+[
+	'Cite web/German',
+	'Cite web/French',
+	'Cite web/Swedish',
+	'Cita libro',
+	'Книга',
+	'Статья',
+	'Literatur'
+].forEach(
+	template_name => task_configuration['Template:' + template_name] = {
+		namespace: 0, for_template, for_each_template_options: {
+			add_index: 'all'
+		}, page_limit: 1
+	}
+);
+
+replace_tool.replace(null, task_configuration);
+
+// ----------------------------------------------------------------------------
 
 // subst展開 [[mw:Help:Substitution]]
 async function for_template(token, index, parent) {
-	//console.log(parent.parent);
-	//console.log(parent.parent?.tag);
-	if (parent.parent?.tag === 'ref') {
+	//this.discard_changes = true;
+	token[0] = 'subst:' + token[0];
+	const page_title = this.page_to_edit.title;
+	//this.task_configuration.wiki.append_session_to_options().session;
+
+	if (CeL.wiki.parser.token_is_children_of(parent,
+		parent => parent.type === 'tag' && (parent.tag === 'ref' || parent.tag === 'gallery')
+	)) {
+		//console.trace([page_title, token.toString(), parent]);
 		// [[mw:Help:Cite#Substitution and embedded parser functions]] [[w:en:Help:Substitution#Limitation]]
 		// refタグ内ではsubst:をつけても展開されず、そのまま残ります。人間による編集の場合は一旦refタグを外して、差分から展開したソースをコピーする形になります。
-		// TODO: this.task_configuration.wiki.expandtemplates()
-		const expand_data = await new Promise(resolve => CeL.wiki.expandtemplates(token.toString(), resolve, this.task_configuration.wiki.append_session_to_options()));
-		//console.log([token.toString(), expand_data]);
-		parent[index] = expand_data.wikitext;
-	} else {
-		token[0] = 'subst:' + token[0];
+
+		// TODO: this.task_configuration.wiki.expandtemplates(), this.task_configuration.wiki.compare()
+		// useless:
+		//const expand_data = await new Promise(resolve => CeL.wiki.query(token.toString(), resolve, this.task_configuration.wiki.append_session_to_options()));
+
+		let wikitext = await this.task_configuration.wiki.query({
+			action: "compare",
+			fromtitle: page_title,
+			fromslots: "main",
+			'fromtext-main': "",
+			toslots: "main",
+			'totext-main': token.toString(),
+			topst: 1,
+		});
+		//console.log(wikitext);
+		wikitext = wikitext.compare['*']
+			// TODO: shoulld use HTML parser
+			.between('<td class="diff-addedline">', '</td>').replace(/^<div>/, '').replace(/<\/div>$/, '');
+		wikitext = CeL.HTML_to_Unicode(wikitext);
+		//console.trace([page_title, token.toString(), wikitext]);
+		parent[index] = wikitext;
 	}
 }
