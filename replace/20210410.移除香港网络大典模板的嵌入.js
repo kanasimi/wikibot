17b0ce8b6f1688b2620d519a0999a16b1c6fb0aa
kanasimi <kanasimi@gmail.com>
2021-05-02 12:09:41 +0800

    + replace/*

---
+++ replace/20210410.移除香港网络大典模板的嵌入.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+// Load replace tools.
+const replace_tool = require('./replace_tool.js');
+
+replace_tool.replace(null, {
+	'Template:Evchk': {
+		move_to_link: DELETE_PAGE,
+		namespace: 0,
+		//allow_empty: true
+	}
+});
---
+++ replace/20210428.insert Panoramio User ID category.js
@@ -0,0 +1,26 @@
+﻿'use strict';
+
+// Load replace tools.
+const replace_tool = require('./replace_tool.js');
+
+replace_tool.replace({
+	language: 'commons',
+	diff_id: 545787087
+}, {
+	"insource:panoramio.com/user/4539074": {
+		text_processor(wikitext, page_data) {
+			const parsed = page_data.parse();
+			let has_token;
+			parsed.each('Category', token => {
+				if (token.name === 'Photos from Panoramio ID 4539074') {
+					has_token = true;
+					return parsed.each.exit;
+				}
+			});
+			if (has_token)
+				return Wikiapi.skip_edit;
+			parsed.insert_layout_token('[[Category:Photos from Panoramio ID 4539074]]');
+			return parsed.toString();
+		}
+	}
+});
