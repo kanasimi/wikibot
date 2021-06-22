3d2491621baf4125b596f629cf3f97992f804d74
kanasimi <kanasimi@gmail.com>
2021-06-23 05:22:32 +0800

    + replace/*

---
+++ replace/20210612.accessdate パラメータの修正.js
@@ -0,0 +1,29 @@
+﻿'use strict';
+
+// Load replace tools.
+const replace_tool = require('./replace_tool.js');
+
+// ----------------------------------------------------------------------------
+
+const PATTERN_date_minus = /(\|\s*[a-z]*date *)-( *[12][0-9]{3})/g;
+
+function text_processor(wikitext) {
+	return PATTERN_date_minus.test(wikitext) ? wikitext.replace(/*this.move_from_link*/PATTERN_date_minus, '$1=$2') : Wikiapi.skip_edit;
+}
+
+replace_tool.replace({
+	requests_page_rvlimit: 800,
+	// Do not use move configuration from section.
+	no_task_configuration_from_section: true,
+}, {
+	'[a-z]*date-year': {
+		list_types: 'search',
+		//move_from_link: PATTERN_date_minus,
+		//move_from_link: /date *- *\d{4}/,
+		//move_from_link: "date-",
+		//move_from_link: /date *- *\d/,
+		//move_from_link: /\|\s*date *-/,
+		move_from_link: /\|accessdate-/,
+		text_processor
+	},
+});
---
+++ replace/replace_tool.archive/20210609.コモンズへの移行が望まれないファイルについてTemplateKeep localを使用するようにする.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+// Load replace tools.
+const replace_tool = require('./replace_tool.js');
+
+// ----------------------------------------------------------------------------
+
+function text_processor(wikitext) {
+	return wikitext.replace(/{{ *注意 *\|[^|{}=]*コモンズ移行後の即時削除を望んでいません[^|{}=]*}}/g, '{{keep local}}');
+}
+
+replace_tool.replace({
+	// Do not use move configuration from section.
+	no_task_configuration_from_section: true,
+}, {
+	'insource:コモンズ移行後の即時削除を望んでいません': { text_processor },
+});
