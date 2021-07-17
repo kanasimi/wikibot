5d2298aca72248c05507e7cefb1156467689255a
kanasimi <kanasimi@gmail.com>
2021-07-18 05:40:34 +0800

    + 20210624.test_subst_Template.js

---
+++ replace/20210624.test_subst_Template.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+
+// Load replace tools.
+const replace_tool = require('./replace_tool.js');
+
+// ----------------------------------------------------------------------------
+
+replace_tool.replace({
+	API_URL: 'test', ignore_language: true,
+	not_bot_requests: true,
+}, {
+	'task 1': 'task 1 done',
+	'Template:L1': 'subst:',
+	'task 2': 'task 2 done',
+});
