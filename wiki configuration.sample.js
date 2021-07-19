1cdd50fd061ed41ef11d8cbc4222c73370d4766a
kanasimi <kanasimi@gmail.com>
2021-07-20 06:03:41 +0800

    + login_options_for_project.zhmoegirl

--- wiki configuration.sample.js
+++ wiki configuration.sample.js
@@ -18,8 +18,22 @@ login_options_for_project[DEFAULT_PROJECT_KEY] = {
 };
 
 // For special wiki projects.
+
+// use_project=lingualibre
 login_options_for_project.lingualibre = {
 	API_URL : 'https://lingualibre.org/api.php',
 	user_name : 'bot name@bot name',
 	password : 'bot password given'
 };
+
+// use_project=zhmoegirl
+login_options_for_project.zhmoegirl = {
+	// API_URL : 'https://zh.moegirl.org.cn/api.php',
+	// 移動端網址的防火牆允許的單位時間內請求數較多，故使用mzh.moegirl.org.cn運行機器人能夠降低撞上防火牆的機率。
+	API_URL : 'https://mzh.moegirl.org.cn/api.php',
+	language : 'zh',
+	site_name : 'zhmoegirl',
+	user_name : 'bot name@bot name',
+	password : 'bot password given'
+};
+
