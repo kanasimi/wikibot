6a367e5e4283391aab0613e277d78428f303a876
kanasimi <kanasimi@gmail.com>
2021-08-11 06:22:50 +0800

    + 20210809.クラス名の誤字修正依頼：nowrapliks→nowraplinks.js

---
+++ replace/replace_tool.archive/20210809.クラス名の誤字修正依頼：nowrapliks→nowraplinks.js
@@ -0,0 +1,24 @@
+﻿'use strict';
+
+// Load replace tools.
+const replace_tool = require('./replace_tool.js');
+
+// ----------------------------------------------------------------------------
+
+const PATTERN_to_replace = /(class="(?:|[^"|]*? ))(nowrapliks)/g;
+
+function text_processor(wikitext) {
+	let changed;
+	wikitext = wikitext.replace_till_stable(PATTERN_to_replace, (all, prefix, class_name) => {
+		changed = true;
+		return prefix + 'nowraplinks';
+	});
+	return changed ? wikitext : Wikiapi.skip_edit;
+}
+
+replace_tool.replace({
+}, {
+	'insource:/[" ]nowrapliks/': {
+		text_processor
+	},
+});
