d0126cd38408698d6ba49424571b81f0b9681d52
kanasimi <kanasimi@gmail.com>
2021-08-17 05:47:44 +0800

    fix summary

--- routine/20210429.Auto-archiver.js
+++ routine/20210429.Auto-archiver.js
@@ -24,7 +24,9 @@ const archive_template_name = 'Auto-archive';
 
 (async () => {
 	await wiki.login(login_options);
-	await wiki.for_each_page(await wiki.embeddedin('Template:' + archive_template_name), for_each_discussion_page);
+	await wiki.for_each_page(await wiki.embeddedin(
+		// wiki.to_namespace(archive_template_name, 'template')
+		'Template:' + archive_template_name), for_each_discussion_page);
 
 	routine_task_done('1d');
 })();
@@ -178,7 +180,9 @@ async function archive_page(configuration) {
 		}
 	}
 
-	const summary = [CeL.wiki.title_link_of('Project:ARCHIVE', use_language === 'zh' ? '歸檔封存作業' : use_language === 'ja' ? '記録保存' : 'Archiving') + ':',
+	const summary = [CeL.wiki.title_link_of(
+		// wiki.to_namespace(archive_template_name, 'template') && 'Project:ARCHIVE'
+		'Template:' + archive_template_name, use_language === 'zh' ? '歸檔封存作業' : use_language === 'ja' ? '記録保存' : 'Archiving') + ':',
 	CeL.wiki.title_link_of(target_root_page), '→', CeL.wiki.title_link_of(archive_to_page)]
 		.join(' ');
 	const summary_tail = ` ${sections_need_to_archive.length} topic(s): ${sections_need_to_archive.map(section => CeL.wiki.title_link_of('#' + section.section_title.link[1])).join(', ')}`;
