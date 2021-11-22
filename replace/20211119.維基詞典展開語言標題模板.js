f028612e56574c1160a13138530631e833106593
kanasimi <kanasimi@gmail.com>
2021-11-23 04:27:10 +0800

    fix async function text_processor(wikitext, page_data, work_config)

--- replace/20211119.維基詞典展開語言標題模板.js
+++ replace/20211119.維基詞典展開語言標題模板.js
@@ -5,6 +5,7 @@
 
 @see
 [[分類:標題行模板]]
+[[分類:語言模板]]
 
  */
 
@@ -18,7 +19,55 @@ const wiki = new Wikiapi;
 
 // ----------------------------------------------------------------------------
 
-const summary_prefix = '[[Special:PermaLink/6463191#章節模板是否應該刪除？|展開語言標題模板]]: ';
+const summary_prefix = '[[Special:PermaLink/6518948#章節模板是否應該刪除？|展開語言標題模板]]: ';
+const main_maintenance_category = '待分類詞彙';
+
+const maintenance_templates = new Set;
+// cache for headers
+const convert_Map = new Map;
+
+(async () => {
+	login_options.API_URL = 'zh.wiktionary';
+	//console.trace(login_options);
+	await wiki.login(login_options);
+
+	const base_task_configuration = { text_processor, list_types: 'embeddedin', template_list_to_process };
+	const move_configuration = Object.create(null);
+	const template_list_to_process = [];
+	await wiki.category_tree('语言模板', {
+		for_each(page_data) {
+			if (!wiki.is_namespace(page_data, 'template')) return;
+			const title = page_data.title;
+
+			// ['-ja-hiragana-', '-fr-', '法語', '-ang-', '越南語漢字詞', '-vi-', '朝鮮語漢字詞', '朝鮮語', '日語-題', '日語', '-en-', '漢語']
+			// for debug: 僅處理單一模板
+			if (!title.includes('馬來語')) return;
+
+			if (title in move_configuration) return;
+			move_configuration[title] = Object.clone(base_task_configuration);
+			template_list_to_process.push(title);
+		}
+	});
+	//template_list_to_process.forEach((template_name, index) => move_configuration[template_list_to_process[index] = wiki.to_namespace(template_name, 'template')] = Object.clone(base_task_configuration));
+
+	//console.log(template_list_to_process);
+	//console.trace(move_configuration);
+	await wiki.category_tree(main_maintenance_category, {
+		for_each(template_page_data) {
+			if (!maintenance_templates.has(template_page_data.title))
+				maintenance_templates.add(template_page_data.title);
+		}
+	});
+	//console.trace(maintenance_templates);
+
+	await wiki.register_redirects(template_list_to_process);
+
+	await replace_tool.replace({
+		log_to: null,
+		not_bot_requests: true,
+		wiki,
+	}, move_configuration);
+})();
 
 async function text_processor(wikitext, page_data, work_config) {
 	const summary = [];
@@ -36,26 +85,44 @@ async function text_processor(wikitext, page_data, work_config) {
 	}
 
 	const categories = new Map;
-	function add_category(category_token) {
+	function register_category(category_token, is_existed) {
 		//console.trace(category_token.name);
-		if (!categories.has(category_token.name)) {
-			categories.set(category_token.name, category_token);
-			append_category(category_token);
+		//console.trace(category_token);
+		console.assert(category_token.type === 'category');
+		let category_name = category_token.name;
+		if (convert_Map.has(category_name)) {
+			// 避免同時存在繁體簡體的 category。
+			category_name = convert_Map.get(category_name);
+			category_token.name = category_name;
+			//category_token[0][1] = category_name;
+			category_token[0] = `Category:${category_name}`;
+		}
+
+		if (!categories.has(category_name)) {
+			categories.set(category_name, category_token);
+			if (!is_existed)
+				append_category(category_token);
 			return;
 		}
+
 		//console.trace(category_token);
 
 		if (!category_token.sort_key)
 			return;
 
-		const old_category_token = categories.get(category_token.name);
+		const old_category_token = categories.get(category_name);
 		//console.trace(old_category_token);
 		if (old_category_token.sort_key) {
-			CeL.warn(`${add_category.name}: ${CeL.wiki.title_link_of(page_data)}: Multiple sort key: ${old_category_token}, ${category_token}`);
-			append_category(category_token);
+			CeL.warn(`${register_category.name}: ${CeL.wiki.title_link_of(page_data)}: Multiple sort key: ${old_category_token}, ${category_token}`);
+			if (!is_existed)
+				append_category(category_token);
 		} else {
 			// reuse old category_token
 			old_category_token[2] = old_category_token.sort_key = category_token.sort_key;
+			if (is_existed) {
+				// 移除重複的/同時存在繁體簡體的 category。
+				return parsed.each.remove_token;
+			}
 		}
 	}
 
@@ -66,7 +133,7 @@ async function text_processor(wikitext, page_data, work_config) {
 		return size < 190 ? `{{huge|${text}}}` : `{{huge|${text}|${size}%}}`;
 	}
 
-	parsed.each('category', category_token => categories.set(category_token.name, category_token));
+	parsed.each('category', category_token => register_category(category_token, true), true);
 	//console.trace(categories);
 
 	function split_category(wikitext) {
@@ -75,11 +142,11 @@ async function text_processor(wikitext, page_data, work_config) {
 
 		const language_list = [];
 		parsed.each('category', category_token => {
-			if (category_token.name === '待分類詞彙' && category_token.sort_key) {
+			if (category_token.name === main_maintenance_category && category_token.sort_key) {
 				language_list.append(category_token.sort_key.split(','));
 				language_list.category_token = category_token;
 			}
-			add_category(category_token);
+			register_category(category_token);
 			return parsed.each.remove_token;
 		}, true);
 
@@ -88,15 +155,6 @@ async function text_processor(wikitext, page_data, work_config) {
 			.replace(/<div style=\"clear: both; height: 1em\"><\/div>/g, '')
 			// remove templatestyles in {{漢字}}, {{vi-nom}}
 			.replace(/<templatestyles src="vi-nom\/fonts.css" \/>/g, '')
-
-			.replace(/<h2>(.+)<\/h2>/g, (all, header) => {
-				header = header
-					.replace(/([英日]|越南)语/g, '$1語')
-					.replace(/汉语/g, '漢語').replace(/朝鲜语/g, '韓語')
-					;
-				language_list.push(header);
-				return `==${header}==`;
-			})
 
 			// for {{越南文}}, {{漢字|越南語漢字|vi}}
 			.replace(/<span class=\"han-nom\" style=\"([^<>]+)>([^<>]*)<\/span>/ig, (all, style, text) => {
@@ -134,6 +192,15 @@ async function text_processor(wikitext, page_data, work_config) {
 		return { wikitext, language_list };
 	}
 
+	const template_list_to_process = this.template_list_to_process;
+
+	function normalize_header(header) {
+		// e.g., {{馬來語}}
+		header = CeL.wiki.wikitext_to_plain_text(header, wiki.append_session_to_options()).trim();
+		//console.trace(header);
+		return header;
+	}
+
 	// https://zh.wiktionary.org/wiki/Special:ApiSandbox#action=expandtemplates&format=json&title=%E9%A6%96%E9%A0%81&text=%7B%7B%E6%BC%A2%E8%AA%9E%7C%E5%AD%97%7D%7D&prop=wikitext&utf8=1
 	// https://zh.wiktionary.org/wiki/Special:ApiSandbox#action=expandtemplates&format=json&title=%E9%A6%96%E9%A0%81&text=%7B%7B-en-%7D%7D&prop=wikitext&utf8=1
 	await parsed.each('template', async template_token => {
@@ -146,13 +213,56 @@ async function text_processor(wikitext, page_data, work_config) {
 				text: template_token.toString()
 			});
 			//console.trace(expanded_data);
-			const converted_data = split_category(expanded_data.expandtemplates.wikitext);
-			const sort_key = converted_data.language_list.unique().join(',') || template_token.name;
+			let wikitext = expanded_data.expandtemplates.wikitext
+				.replace(/<h2>(.+)<\/h2>/g, `==$1==`);
+
+			const header_list = [];
+			function add_header(PATTERN) {
+				for (const matched of wikitext.matchAll(PATTERN)) {
+					const header = normalize_header(matched.groups.header);
+					if (!convert_Map.has(header))
+						header_list.push(header);
+				}
+			}
+			add_header(/(==)(?<header>.+)\1/g);
+			//add_header(/<h2>(.+)<\/h2>/g);
+			if (header_list.length > 0) {
+				const converted_header_list = await wiki.convert_Chinese(header_list, { uselang: 'zh-hant' });
+				header_list.forEach((header, index) => convert_Map.set(header, converted_header_list[index]));
+				//console.trace(convert_Map);
+			}
+			// free
+			//header_list.truncate();
+
+			const language_list = [];
+			wikitext = wikitext.replace(/==(.+)==/g, (all, header) => {
+				header = convert_Map.get(normalize_header(header));
+				console.assert(!!header);
+				//console.trace(header);
+				language_list.push(header);
+
+				const category_name = wiki.to_namespace(`待分類的${header}詞`, 'category');
+				if (!maintenance_templates.has(category_name)) {
+					maintenance_templates.add(category_name);
+					/*await*/ wiki.edit_page(category_name, `{{Hidden category}}
+
+[[${wiki.to_namespace(main_maintenance_category, 'category')}]]
+`, {
+						summary: summary_prefix + `創建追縱、維護分類`
+					});
+				}
+				register_category(CeL.wiki.parse(`[[${category_name}]]`, wiki.append_session_to_options()));
+				return `==${header}==`;
+			});
+			//console.trace(wikitext);
+
+			const converted_data = split_category(wikitext);
+			const sort_key = language_list.append(converted_data.language_list).unique().join(',') || template_token.name;
 			if (converted_data.language_list.category_token) {
 				// reuse old category_token
 				converted_data.language_list.category_token[2] = converted_data.language_list.category_token.sort_key = sort_key;
 			} else {
-				add_category(CeL.wiki.parser(`[[Category:待分類詞彙|${sort_key}]]`, wiki.append_session_to_options()).parse());
+				//register_category(CeL.wiki.parse(`[[${wiki.to_namespace(main_maintenance_category,'category')}|${sort_key}]]`, wiki.append_session_to_options()));
 			}
 			//console.trace(converted_data);
 			return converted_data.wikitext;
@@ -171,25 +281,3 @@ async function text_processor(wikitext, page_data, work_config) {
 		.replace(/((?:\n|^)(={2})[^=].*?\2)(?! *\n)/g, '$1\n')
 		;
 }
-
-// [[分類:語言模板]]
-const template_list_to_process = ['越南語漢字詞', '-vi-', '朝鮮語漢字詞', '朝鮮語', '日語-題', '日語', '-en-', '漢語'];
-
-(async () => {
-	login_options.API_URL = 'zh.wiktionary';
-	//console.trace(login_options);
-	await wiki.login(login_options);
-
-	const base_task_configuration = { text_processor, list_types: 'embeddedin' };
-	const move_configuration = Object.create(null);
-	template_list_to_process.forEach((template_name, index) => move_configuration[template_list_to_process[index] = wiki.to_namespace(template_name, 'template')] = Object.clone(base_task_configuration));
-	//console.trace(move_configuration);
-
-	await wiki.register_redirects(template_list_to_process);
-
-	await replace_tool.replace({
-		log_to: null,
-		not_bot_requests: true,
-		wiki,
-	}, move_configuration);
-})();
