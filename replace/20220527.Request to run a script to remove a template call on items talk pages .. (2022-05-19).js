e7db823875993a7a345b1dc9e3781854fb0bd9a3
kanasimi <kanasimi@gmail.com>
2022-06-08 05:13:09 +0800

    +20220527.Request to run a script to remove a template call on items talk pages .. (2022-05-19).js

---
+++ replace/20220527.Request to run a script to remove a template call on items talk pages .. (2022-05-19).js
@@ -0,0 +1,24 @@
+﻿'use strict';
+
+// Load replace tools.
+const replace_tool = require('./replace_tool.js');
+
+// ----------------------------------------------------------------------------
+
+replace_tool.replace({
+	API_URL: 'wikidata',
+}, {
+	'Template:Item documentation': {
+		namespace: 'talk',
+		move_to_link: DELETE_PAGE,
+		list_types: 'embeddedin',
+		_text_processor(wikitext) {
+			// e.g., [[Talk:Q11457]]
+			const replace_to = wikitext.replace(/^([\s\n]*{{[\s\S]+?}})?[\s\n]*{{ *[Ii]tem[ _][Dd]ocumentation(?:\s*\|[^{}]*)?}}[\s\n]*/, '$1');
+			//console.log([wikitext, replace_to]);
+			if (replace_to !== wikitext) {
+				return replace_to;
+			}
+		}
+	},
+});
