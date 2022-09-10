4c7e2dce24899a2c40edff2f96fe547819b01bb9
kanasimi <kanasimi@gmail.com>
2022-09-11 06:04:06 +0800

    + 20220908.請求移除 TemplateTaxonomy 所有子模板中的冗餘代碼「{{{1}}}」.js

---
+++ replace/replace_tool.archive/20220908.請求移除 TemplateTaxonomy 所有子模板中的冗餘代碼「{{{1}}}」.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+// Load replace tools.
+const replace_tool = require('./replace_tool.js');
+
+// ----------------------------------------------------------------------------
+
+replace_tool.replace({
+}, {
+	//'prefix:Template:Taxonomy/'
+	'Category:分类学模板': {
+		//namespace: 'template',
+		text_processor(wikitext) {
+			return wikitext.replace("{{Don't edit this line {{{machine code|}}}|{{{1}}}", "{{Don't edit this line {{{machine code|}}}");
+		}
+	},
+});
