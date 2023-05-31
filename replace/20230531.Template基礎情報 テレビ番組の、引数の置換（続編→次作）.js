e585ccfa9631a3da2d801738e97c83e00df45bc9
kanasimi <kanasimi@gmail.com>
2023-06-01 05:40:19 +0800

    archive samples

---
+++ replace/20230531.Template基礎情報 テレビ番組の、引数の置換（続編→次作）.js
@@ -0,0 +1,24 @@
+﻿/*
+
+2023/5/31 15:49:33	初版試營運
+
+ */
+
+'use strict';
+
+// Load replace tools.
+const replace_tool = require('./replace_tool.js');
+//import { replace } from './replace_tool.js';
+
+// ----------------------------------------------------------------------------
+
+//async function main_process()
+(async () => {
+	await replace_tool.replace(null, {
+		'Template:基礎情報 テレビ番組': {
+			for_template(token) {
+				CeL.wiki.parse.replace_parameter(token, { 続編: '次作' }, 'parameter_name_only');
+			}
+		}
+	});
+})();
