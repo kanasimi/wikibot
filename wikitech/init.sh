636eb78b11557791a7c3c0ed8a18166f6a08e033
kanasimi <kanasimi@gmail.com>
2025-07-01 08:32:22 +0800

    Nothing seems to have `wget`, but everything has `curl`. Apparently we have `unzip` in bookworm based images. That should include the "node18" image.

--- wikitech/init.sh
+++ wikitech/init.sh
@@ -41,19 +41,25 @@ cd node_modules
 # /usr/bin/unzip -UU : 以 jsub @crontab 執行的時候會出現亂碼
 # sh has no `time`, so we can not use `time /usr/bin/unzip` @ Debian Stretch 2019/3/15
 # bash hash `time`
-/usr/bin/wget -O CeJS.zip https://github.com/kanasimi/CeJS/archive/master.zip && [ -d cejs ] && /usr/bin/diff CeJS.zip CeJS.zip.old && mv -f CeJS.zip CeJS.zip.old && echo "CeJS: No news." || ( echo "Extracting CeJS..." && /usr/bin/unzip CeJS.zip > /dev/null && mv -f CeJS.zip CeJS.zip.old && rm -rf cejs && mv CeJS-master cejs && (cp -f "cejs/_for include/_CeL.loader.nodejs.js" ~/wikibot) && ( [ -d opencc/data/dictionary ] && cp -f opencc/data/dictionary/* cejs/extension/zh_conversion/OpenCC/ || echo "No OpenCC!" ) || echo "Failed to get CeJS!" )
+
+# node18 starts from our bookworm base and then adds nodejs and a fewmore bits.
+# everything has `curl`. Apparently we have `unzip` in bookworm based images. That should include the "node18" image.
+
+/usr/bin/curl --location --output CeJS.zip https://github.com/kanasimi/CeJS/archive/master.zip && [ -d cejs ] && /usr/bin/diff CeJS.zip CeJS.zip.old && mv -f CeJS.zip CeJS.zip.old && echo "CeJS: No news." || ( echo "Extracting CeJS..." && /usr/bin/unzip CeJS.zip > /dev/null && mv -f CeJS.zip CeJS.zip.old && rm -rf cejs && mv CeJS-master cejs && (cp -f "cejs/_for include/_CeL.loader.nodejs.js" ~/wikibot) && ( [ -d opencc/data/dictionary ] && cp -f opencc/data/dictionary/* cejs/extension/zh_conversion/OpenCC/ || echo "No OpenCC!" ) || echo "Failed to get CeJS!" )
+
 
 # ---------------------------------------------------------
 # 2019/9/12 18:25:17
 
 printf "$SP\nUpdate wikiapi...\n"
 
-/usr/bin/wget -O wikiapi.zip https://github.com/kanasimi/wikiapi/archive/master.zip && [ -d wikiapi ] && /usr/bin/diff wikiapi.zip wikiapi.zip.old && mv -f wikiapi.zip wikiapi.zip.old && echo "wikiapi: No news." || ( echo "Extracting wikiapi..." && /usr/bin/unzip wikiapi.zip > /dev/null && mv -f wikiapi.zip wikiapi.zip.old && rm -rf wikiapi && mv wikiapi-master wikiapi || echo "Failed to get CeJS" )
+/usr/bin/curl --location --output wikiapi.zip https://github.com/kanasimi/wikiapi/archive/master.zip && [ -d wikiapi ] && /usr/bin/diff wikiapi.zip wikiapi.zip.old && mv -f wikiapi.zip wikiapi.zip.old && echo "wikiapi: No news." || ( echo "Extracting wikiapi..." && /usr/bin/unzip wikiapi.zip > /dev/null && mv -f wikiapi.zip wikiapi.zip.old && rm -rf wikiapi && mv wikiapi-master wikiapi || echo "Failed to get CeJS" )
 
 # ---------------------------------------------------------
 
 printf "$SP\nCopy task programs...\n"
 
+#cd $HOME
 cd ~
 
 #if [ -d "wikibot" ]; then
@@ -75,7 +81,8 @@ cd ~
 
 # method 2:
 # /usr/bin/unzip -UU
-/usr/bin/wget -O wikibot.zip https://github.com/kanasimi/wikibot/archive/master.zip && [ -d wikibot ] && /usr/bin/diff wikibot.zip wikibot.zip.old && mv -f wikibot.zip wikibot.zip.old && echo "wikibot: No news." || ( echo "Extracting wikibot..." && /usr/bin/unzip wikibot.zip > /dev/null && mv -f wikibot.zip wikibot.zip.old && rsync -a --remove-source-files wikibot-master/ wikibot && rm -rf wikibot-master || echo "Failed to get wikibot" )
+
+/usr/bin/curl --location --output wikibot.zip https://github.com/kanasimi/wikibot/archive/master.zip && [ -d wikibot ] && /usr/bin/diff wikibot.zip wikibot.zip.old && mv -f wikibot.zip wikibot.zip.old && echo "wikibot: No news." || ( echo "Extracting wikibot..." && /usr/bin/unzip wikibot.zip > /dev/null && mv -f wikibot.zip wikibot.zip.old && rsync -a --remove-source-files wikibot-master/ wikibot && rm -rf wikibot-master || echo "Failed to get wikibot" )
 
 [ -e wikibot/*#U* ] && /bin/rm wikibot/*#U*
 
@@ -87,6 +94,7 @@ printf "$SP\nCreate links...\n"
 
 [ -e ~/bin/node ] || ln -s /shared/bin/node ~/bin/
 
+#cd ${HOME}/wikibot
 cd ~/wikibot
 
 [ -e "js.js" ] || ln -s ../node_modules/cejs/application/net/wiki.js js.js
